63f09f3fe2a3aa5de49a48d27b70285993ca784e
Leandro Palmeira <leandropalmeira@hotmail.com>
2024-08-16 00:31:09 -0300

    Update index.js

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -36,7 +36,7 @@ const useStyles = makeStyles(theme => ({
 		width: "100vw",
 		height: "100vh",
 		//background: "linear-gradient(to right, #76EE00 , #76EE00 , #458B00)",
-		backgroundImage: "url(https://i.imgur.com/CGby9tN.png)",
+		backgroundImage: "url(https://i.imgur.com/gB0s3Zf.jpeg)",
 		backgroundRepeat: "no-repeat",
 		backgroundSize: "100% 100%",
 		backgroundPosition: "center",
